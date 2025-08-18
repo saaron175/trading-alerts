ab9d45143c9fca9033549625ae982083631fd045
saaron175 <s.aaron23@yahoo.com>
2025-08-18 15:55:44 -0500

    Delete telegram_bot.py

--- telegram_bot.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-TOKEN = "8268164684:AAGgUf_tt1MnilRJayLphEv5YLvbpnF453M"
-CHAT_ID = "8437423038"
-
-def send_telegram_message(message):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    payload = {"chat_id": CHAT_ID, "text": message}
-    requests.post(url, data=payload)
